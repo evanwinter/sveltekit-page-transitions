256d05dfc2851a48ea323ee9d0a33a1c52a74daf
Evan Winter <evanwinter@Evans-MacBook-Pro.local>
2021-05-14 16:41:56 -0500

    Add netlify adapter, for demo site

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,11 @@
+import netlify from "@sveltejs/adapter-netlify"
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: netlify()
 	}
 };
 
