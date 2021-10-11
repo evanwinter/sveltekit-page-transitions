e5c267c76b1572b2f5e575f00d7f6f1b995899a8
Evan Winter <evanwinter@Evans-MacBook-Pro.local>
2021-10-10 21:58:13 -0500

    Update dependencies, netlify.toml file and home page copy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>SvelteKit Page Transition Example</h1>
+<p>
+  Navigate between pages using the links above, and an animated page transition
+  will occur.
+</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import netlify from "@sveltejs/adapter-netlify"
+import adapter from "@sveltejs/adapter-netlify"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: netlify()
+		adapter: adapter()
 	}
 };
 
