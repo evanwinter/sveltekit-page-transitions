8e2b80eb784b234e595112cfe792aac94edfd191
Evan Winter <evan@evanwinter.me>
2022-12-16 23:52:30 -0600

    chore: Update to SvelteKit 1.0+ and add web monetization

--- src/lib/components/PageTransition.svelte
+++ src/lib/components/PageTransition.svelte
@@ -4,7 +4,7 @@
 </script>
 
 {#key pathname}
-	<div in:fly={{ x: -5, duration: 500, delay: 500 }} out:fly={{ x: 5, duration: 500 }}>
+	<div in:fly={{ x: -10, duration: 500, delay: 500 }} out:fly={{ x: 5, duration: 500 }}>
 		<slot />
 	</div>
 {/key}
